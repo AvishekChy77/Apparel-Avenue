3117855052a595b1249294f8c2ee919b7960f653
AvishekChy77 <avishekchowdhury71@gmail.com>
2024-10-20 16:03:00 +0600

    added banner slider

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,34 @@
+import { A11y, Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import slide1 from "../../../public/banner_1.png";
+import slide2 from "../../../public/banner_2.png";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+const Banner = () => {
+  return (
+    <div className="">
+      <Swiper
+        // install Swiper modules
+        modules={[Autoplay, Pagination, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <img className="h-[90vh] w-full object-cover" src={slide1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className=" h-[90vh] w-full object-cover" src={slide2} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../Shared/Navbar";
 import AllBrands from "./AllBrands";
 import Introduction from "./Introduction";
 import Reviews from "./Reviews";
+import Banner from "./Banner";
 
 const Home = () => {
   const brands = useLoaderData();
@@ -12,26 +13,7 @@ const Home = () => {
     <div>
       <Navbar></Navbar>
 
-      {/* banner */}
-      <div
-        className="w-full flex items-center h-56 md:h-96 relative mb-5 md:my-7 lg:h-[85vh]"
-        style={{
-          backgroundImage: "url(https://i.ibb.co/sywrLVk/image-3.png)",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      >
-        <div className=" absolute left-2 md:left-10  max-w-xl text-center space-y-2">
-          <h2 className=" font-extrabold text-2xl md:text-4xl lg:text-6xl">
-            Winter Blust
-          </h2>
-          <p className=" text-sm md:text-xl lg:text-3xl">
-            {" "}
-            Flat 20% Discount on all porducts
-          </p>
-        </div>
-      </div>
+      <Banner />
       <Introduction></Introduction>
       <AllBrands brands={brands}></AllBrands>
       <Reviews></Reviews>
